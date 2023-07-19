1a1798273f308268e131ce5ab463eb6d58396c40
Trieu Vu <vutrieu2002@gmail.com>
2023-07-19 12:04:24 +0700

    optimized sortByAscending in MyCaculator.java

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -1,18 +1,39 @@
 public class MyCalculator {
 
-    // sort using 'buble sort', time complexity is O(N^2)
+    // sort using 'quicksort', time complexity is O(NlogN)
     public static void sortByAscending(int[] arr) {
-        // TODO: using other algorithms better
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
         }
     }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 
     public static String stringConcatenator(String str1, String str2,
                                             String str3, String str4, String str5) {
