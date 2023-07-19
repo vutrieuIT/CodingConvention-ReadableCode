6958790d4ed94e2d2b2c6885e121e62338315e0f
Trieu Vu <vutrieu2002@gmail.com>
2023-07-19 11:47:19 +0700

    code convention: formating in Main.java using ide tool

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
-class employee{
-private static final String company = "RUNSYSTEM";
-String a;
-// print name employee
-void P(){
-    System.out.println("name is " + a);
-}
-}
-public static void main(String[] args) {
-System.out.println("Hello world!");
-}
+    class employee {
+        private static final String company = "RUNSYSTEM";
+        String a;
+
+        // print name employee
+        void P() {
+            System.out.println("name is " + a);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
